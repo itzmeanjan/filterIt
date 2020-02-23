267b8abb49d01a63f8c640003fbead4aa3c0fee7
Anjan Roy <anjanroy@yandex.com>
2020-02-23 19:04:13 +0530

    generated upto order 5 mean, median & mode filtered images of original image; put images on readme;

--- Driver.java
+++ Driver.java
@@ -1,20 +1,76 @@
+import java.io.File;
+
 class Driver {
+
+    // given a image file name, obtains extension of that image
+    String imageExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+    void usage() {
+        System.out.println("\n[+] Usage : java Driver `image.(jpg | png)` order ( <= filters to be applied )\n");
+    }
+
+    // this program requires user to pass
+    // target image file name as command line argument
+    // which is required to be a valid file name
+    // present on system
+    // if so, we'll return that name, else returns null
+    String getFileFromCMD(String[] args) {
+        try {
+            if (args.length != 2) {
+                return null;
+            }
+            File file = new File(args[0]);
+            return file.exists() && (file.getName().endsWith(".jpg") || file.getName().endsWith(".png")) ? args[0]
+                    : null;
+        } catch (Exception e) {
+            System.err.println(e);
+            return null;
+        }
+    }
+
+    int getOrderFromCMD(String[] args) {
+        try {
+            return args.length != 2 ? 0 : Integer.parseInt(args[1]);
+        } catch (Exception e) {
+            System.err.println(e);
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
-        Thread[] thrdArr = new Thread[6];
+        Driver driver = new Driver();
+        String fileName = driver.getFileFromCMD(args);
+        if (fileName == null) {
+            driver.usage();
+            System.exit(-1);
+        }
+        int order = driver.getOrderFromCMD(args);
+        if (order == 0) {
+            System.err.println("[!] Invalid order value");
+            System.exit(-1);
+        }
+        Thread[] thrdArr = new Thread[3 * order];
         int idx = 0;
-        for (int i = 1; i < 3; i++) {
-            thrdArr[idx] = new Thread(new Worker(new MeanFilter("sample.jpg"), "order_" + i + "_MeanFiltered.jpg", i),
-                    "Thread_" + (idx + 1));
-            idx++;
-        }
-        for (int i = 1; i < 3; i++) {
+        for (int i = 1; i <= order; i++) {
             thrdArr[idx] = new Thread(
-                    new Worker(new MedianFilter("sample.jpg"), "order_" + i + "_MedianFiltered.jpg", i),
+                    new Worker(new MeanFilter(fileName),
+                            "order_" + i + "_MeanFiltered." + driver.imageExtension(fileName), i),
                     "Thread_" + (idx + 1));
             idx++;
         }
-        for (int i = 1; i < 3; i++) {
-            thrdArr[idx] = new Thread(new Worker(new ModeFilter("sample.jpg"), "order_" + i + "_ModeFiltered.jpg", i),
+        for (int i = 1; i <= order; i++) {
+            thrdArr[idx] = new Thread(
+                    new Worker(new MedianFilter(fileName),
+                            "order_" + i + "_MedianFiltered." + driver.imageExtension(fileName), i),
+                    "Thread_" + (idx + 1));
+            idx++;
+        }
+        for (int i = 1; i <= order; i++) {
+            thrdArr[idx] = new Thread(
+                    new Worker(new ModeFilter(fileName),
+                            "order_" + i + "_ModeFiltered." + driver.imageExtension(fileName), i),
                     "Thread_" + (idx + 1));
             idx++;
         }
@@ -28,6 +84,7 @@ class Driver {
                 System.err.println(ie);
             }
         }
-        System.out.println("[+]Done");
+        System.out.println("\n[+]Done");
+        System.exit(0);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # filterIt
 Another Image Filtering implementation in Java
+
+## mean filter
+
+Image Type | Image
+--- | ---
+Original | ![sample_image](sample.jpg)
+Order 1 | ![order_1_MeanFiltered](order_1_MeanFiltered.jpg)
+Order 2 | ![order_2_MeanFiltered](order_2_MeanFiltered.jpg)
+Order 3 | ![order_3_MeanFiltered](order_3_MeanFiltered.jpg)
+Order 4 | ![order_4_MeanFiltered](order_4_MeanFiltered.jpg)
+Order 5 | ![order_5_MeanFiltered](order_5_MeanFiltered.jpg)
+
+
+## median filter
+
+Image Type | Image
+--- | ---
+Original | ![sample_image](sample.jpg)
+Order 1 | ![order_1_MedianFiltered](order_1_MedianFiltered.jpg)
+Order 2 | ![order_2_MedianFiltered](order_2_MedianFiltered.jpg)
+Order 3 | ![order_3_MedianFiltered](order_3_MedianFiltered.jpg)
+Order 4 | ![order_4_MedianFiltered](order_4_MedianFiltered.jpg)
+Order 5 | ![order_5_MedianFiltered](order_5_MedianFiltered.jpg)
+
+## mode filter
+
+Image Type | Image
+--- | ---
+Original | ![sample_image](sample.jpg)
+Order 1 | ![order_1_ModeFiltered](order_1_ModeFiltered.jpg)
+Order 2 | ![order_2_ModeFiltered](order_2_ModeFiltered.jpg)
+Order 3 | ![order_3_ModeFiltered](order_3_ModeFiltered.jpg)
+Order 4 | ![order_4_ModeFiltered](order_4_ModeFiltered.jpg)
+Order 5 | ![order_5_ModeFiltered](order_5_ModeFiltered.jpg)
+
+**Thanking you ...**
