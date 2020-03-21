dc8e36ed10901472a0fef127252a80d64f7f4132
Anjan Roy <anjanroy@yandex.com>
2020-03-21 11:31:09 +0530

    added code comment in contrast stretching implementation;

--- ContrastStretching.java
+++ ContrastStretching.java
@@ -1,7 +1,19 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
+/**
+ * Contrast Stretching is pretty similar to Histogram Equalization, but rather
+ * that computing pixel intensity probability & corresponding CDF values, we'll
+ * go for stretching pixel intensity range, which may be intially `a` & `b`,
+ * where a > 0 && / || b < 255, but we'll make it 0 to 255. Eventually image
+ * will get brighter.
+ */
 class ContrastStretching {
+
+    /**
+     * Given a grayscale image, which is already read into BufferedImage, we'll find
+     * minimum pixel intensity ( >= 0)
+     */
     private int minIntensity(BufferedImage img) {
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < img.getHeight(); i++) {
@@ -14,6 +26,10 @@ class ContrastStretching {
         return min;
     }
 
+    /**
+     * Same as previous one, but we'll rather find maximum intensity value, which
+     * must be <=255
+     */
     private int maxIntensity(BufferedImage img) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < img.getHeight(); i++) {
@@ -26,11 +42,22 @@ class ContrastStretching {
         return max;
     }
 
+    /**
+     * We'll transform pixel intensity value using stretching function & return
+     * transformed value ( >= 0 && <= 255 ), which will be put into (x, y) position
+     * of modified image buffer
+     */
     private int transformPixel(int minIntensity, int maxIntensity, int intensity, int maxPossibleIntensity) {
         return (int) (maxPossibleIntensity
                 * ((double) (intensity - minIntensity) / (double) (maxIntensity - minIntensity)));
     }
 
+    /**
+     * For each pixel intensity value of a grayscale image, we'll apply afore
+     * defined transformation function & put transformed pixel intensity value in
+     * each cell, which will be returned ( that can be either exported or processed
+     * further )
+     */
     BufferedImage transform(BufferedImage img) {
         if (img == null) {
             return null;
@@ -50,6 +77,11 @@ class ContrastStretching {
         return transformed;
     }
 
+    /**
+     * Just another way to invoke previous transformation function, instead of
+     * passing BufferedImage, image file name to be passed, which will be read &
+     * modified image buffer to be returned ( using previous function )
+     */
     BufferedImage transform(String src) {
         return this.transform(ImportExportImage.importImage(src));
     }
