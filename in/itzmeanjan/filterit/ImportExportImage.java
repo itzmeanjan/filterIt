f6f9d0bdf86d9477040966b9ea4ac74a37fc48b2
Anjan Roy <anjanroy@yandex.com>
2020-05-07 10:20:46 +0530

    wrote function for creating image with specified color values;

--- in/itzmeanjan/filterit/ImportExportImage.java
+++ in/itzmeanjan/filterit/ImportExportImage.java
@@ -11,45 +11,66 @@ import javax.imageio.ImageIO;
  * image file
  */
 public class ImportExportImage {
-  /** Reads content of specified image file into a BufferedImage object */
-  public static BufferedImage importImage(String src) {
-    try {
-      return ImageIO.read(new File(src));
-    } catch (IOException io) {
-      return null;
+    /**
+     * Reads content of specified image file into a BufferedImage object
+     */
+    public static BufferedImage importImage(String src) {
+        try {
+            return ImageIO.read(new File(src));
+        } catch (IOException io) {
+            return null;
+        }
     }
-  }
 
-  /** Exports content of BufferedImage into a sink file */
-  public static ReturnVal exportImage(BufferedImage img, String sink) {
-    try {
-      if (img == null) throw new IOException("Null image buffer");
-      ImageIO.write(img, ImportExportImage.imageExtension(sink), new File(sink));
-      return new ReturnVal(0, "success");
-    } catch (IOException io) {
-      return new ReturnVal(1, io.toString());
+    /**
+     * Exports content of BufferedImage into a sink file
+     */
+    public static ReturnVal exportImage(BufferedImage img, String sink) {
+        try {
+            if (img == null) throw new IOException("Null image buffer");
+            ImageIO.write(img, ImportExportImage.imageExtension(sink), new File(sink));
+            return new ReturnVal(0, "success");
+        } catch (IOException io) {
+            return new ReturnVal(1, io.toString());
+        }
     }
-  }
 
-  /** Given an image file name, obtains extension of that image i.e. jpg / png */
-  public static String imageExtension(String fileName) {
-    return fileName.substring(fileName.lastIndexOf(".") + 1);
-  }
-
-  /**
-   * Given a buffered image, we'll modify it by setting each pixel with
-   * provided color, could be helpful in setting background of image & then draw on it.
-   *
-   * @param img Image to be modified
-   * @param color Color value to be set at each pixel location across image I
-   * @return Returns modified image buffer, in place editing performed
-   */
-  public static BufferedImage setCanvas(BufferedImage img, Color color){
-    for(int i = 0; i< img.getHeight(); i++){
-      for(int j = 0; j< img.getWidth(); j++){
-        img.setRGB(j, i, color.getRGB());
-      }
+    /**
+     * Given an image file name, obtains extension of that image i.e. jpg / png
+     */
+    public static String imageExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
-    return img;
-  }
+
+    /**
+     * Given a buffered image, we'll modify it by setting each pixel with
+     * provided color, could be helpful in setting background of image & then draw on it.
+     *
+     * @param img   Image to be modified
+     * @param color Color value to be set at each pixel location across image I
+     * @return Returns modified image buffer, in place editing performed
+     */
+    public static BufferedImage setCanvas(BufferedImage img, Color color) {
+        for (int i = 0; i < img.getHeight(); i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                img.setRGB(j, i, color.getRGB());
+            }
+        }
+        return img;
+    }
+
+    /**
+     * Generates a RGB buffered image of specified size,
+     * with each pixel set to given color
+     *
+     * @param width  Width of image to be generated
+     * @param height Height of image to be generated
+     * @param color  Color at each pixel on image
+     * @return Buffered RGB image with pixels set to specified color intensity value
+     */
+    public static BufferedImage prepareCanvas(int width, int height, Color color) {
+        return ImportExportImage.setCanvas(
+                new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR),
+                color);
+    }
 }
