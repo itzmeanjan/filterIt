631b157aee61a2f04db7d6c6847a14a62d33521a
Anjan Roy <anjanroy@yandex.com>
2020-05-16 23:02:43 +0530

    Improved Min Filter
    - Previously MinFilter was processing each pixel location concurrently, which
    was causing an inflation in cost of computation ( due to increased communication cost
    needed to be made in case of very large images ) i.e. each worker was doing little work,
    but we were creating a lot of short living workers.
    - So, I remodeled it into **processing each row of image matrix** concurrently.

--- in/itzmeanjan/filterit/filter/MinFilter.java
+++ in/itzmeanjan/filterit/filter/MinFilter.java
@@ -1,7 +1,8 @@
 package in.itzmeanjan.filterit.filter;
 
 import in.itzmeanjan.filterit.ImportExportImage;
-import in.itzmeanjan.filterit.Pixel;
+import in.itzmeanjan.filterit.segmentation.Image;
+
 import java.awt.image.BufferedImage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,49 +15,55 @@ import java.util.concurrent.TimeUnit;
  */
 public class MinFilter implements Filter {
 
-  /**
-   * Given a buffered image, computes min pixel intensity from neighbourhood of that pixel, and
-   * replaces so in sink image. Each pixel gets processed concurrently, on different therads of
-   * execution
-   */
-  @Override
-  public BufferedImage filter(BufferedImage img, int order) {
-    if (img == null) {
-      return null;
+    /**
+     * Given a buffered image, computes min pixel intensity from neighbourhood of that pixel, and
+     * replaces so in sink image. Each row of image matrix gets processed concurrently,
+     * possibly on different thread of execution i.e. thread available at pool
+     */
+    @Override
+    public BufferedImage filter(BufferedImage img, int order) {
+        if (img == null) {
+            return null;
+        }
+        ExecutorService eService =
+                Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+        Image image = Image.fromBufferedImage(img);
+        for (int i = 0; i < img.getHeight(); i++) {
+            eService.execute(
+                    new MinFilterWorker(
+                            order,
+                            i,
+                            image,
+                            result
+                    )
+            );
+        }
+        eService.shutdown();
+        try {
+            // waiting for all of those workers to complete their tasks
+            eService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException ie) {
+            eService.shutdownNow();
+            result = null;
+        }
+        return result;
     }
-    ExecutorService eService =
-        Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
-    for (int i = 0; i < img.getHeight(); i++) {
-      for (int j = 0;
-          j < img.getWidth();
-          eService.execute(
-              new MinFilterWorker(
-                  img, result, new Pixel(img.getWidth(), img.getHeight(), i, j++), order))) ;
-    }
-    eService.shutdown();
-    try {
-      // waiting for all of those workers to complete their tasks
-      eService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-    } catch (InterruptedException ie) {
-      eService.shutdownNow();
-      result = null;
-    }
-    return result;
-  }
 
-  /**
-   * Given path to image file & order of filtering ( > 0 ), we'll compute min filtered image, to be
-   * returned in buffered image form, which can be processed further, if desired.
-   */
-  @Override
-  public BufferedImage filter(String src, int order) {
-    return this.filter(ImportExportImage.importImage(src), order);
-  }
+    /**
+     * Given path to image file & order of filtering ( > 0 ), we'll compute min filtered image, to be
+     * returned in buffered image form, which can be processed further, if desired.
+     */
+    @Override
+    public BufferedImage filter(String src, int order) {
+        return this.filter(ImportExportImage.importImage(src), order);
+    }
 
-  /** Obtains name of this specific filter */
-  @Override
-  public String filterName() {
-    return "Min Filter";
-  }
+    /**
+     * Obtains name of this specific filter
+     */
+    @Override
+    public String filterName() {
+        return "Min Filter";
+    }
 }
