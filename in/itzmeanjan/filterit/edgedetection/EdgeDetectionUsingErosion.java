e2fe0f8d374cf92bec144ee530cc3fa030258939
Anjan Roy <anjanroy@yandex.com>
2020-05-17 12:39:24 +0530

    Code commenting on Edge detection using erosion

--- in/itzmeanjan/filterit/edgedetection/EdgeDetectionUsingErosion.java
+++ in/itzmeanjan/filterit/edgedetection/EdgeDetectionUsingErosion.java
@@ -7,8 +7,26 @@ import in.itzmeanjan.filterit.morphology.Erosion;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Detects edges of image by applying combination of erosion & subtraction.
+ * Only grayscale images supported, so implicit gray scaling to be performed,
+ * inside this implementation.
+ * - Applies gray scaling on given image
+ * <p>
+ * - Then erodes that image for 1 time
+ * <p>
+ * - Finally subtracts eroded image from gray scaled one
+ * <p>
+ * - And we've our resulting edge detected image
+ */
 public class EdgeDetectionUsingErosion {
 
+    /**
+     * Detects edges of image
+     *
+     * @param img Image on which edges to be detected
+     * @return Edge detected image
+     */
     public BufferedImage detect(BufferedImage img) {
         if (img == null) {
             return null;
@@ -17,6 +35,12 @@ public class EdgeDetectionUsingErosion {
         return new Subtraction().operate(gray, new Erosion().erode(gray, 1), true);
     }
 
+    /**
+     * Detects edges of image
+     *
+     * @param img Image on which edges to be detected
+     * @return Edge detected image
+     */
     public BufferedImage detect(String img) {
         return this.detect(ImportExportImage.importImage(img));
     }
