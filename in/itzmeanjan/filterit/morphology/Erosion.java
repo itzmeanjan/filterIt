f05227b4c8f1f97a060087c642d6045715ecb76e
Anjan Roy <anjanroy@yandex.com>
2020-05-17 12:20:59 +0530

    Updated Erosion implementation
    - Added iterative erosion application option in Erosion class
    - Added code doc in Erosion implementation

--- in/itzmeanjan/filterit/morphology/Erosion.java
+++ in/itzmeanjan/filterit/morphology/Erosion.java
@@ -5,13 +5,43 @@ import in.itzmeanjan.filterit.filter.MinFilter;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Given an image it'll try to shrink foreground region of image, where
+ * foreground denoted by white color ( higher pixel intensity )
+ * <p>
+ * It's nothing but application of min filter on black-white image.
+ */
 public class Erosion {
 
-    public BufferedImage erode(BufferedImage img) {
-        return new MinFilter().filter(img, 1);
+
+    /**
+     * Erodes given image
+     *
+     * @param img Image to be eroded
+     * @param itr Times erosion to be performed iteratively
+     * @return Eroded image
+     */
+    public BufferedImage erode(BufferedImage img, int itr) {
+        if (img == null || itr < 1) {
+            return null;
+        }
+        BufferedImage sink = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+        sink.setData(img.getData());
+        MinFilter minFilter = new MinFilter();
+        for (int i = 0; i < itr; i++) {
+            sink = minFilter.filter(sink, 1);
+        }
+        return sink;
     }
 
-    public BufferedImage erode(String img) {
-        return this.erode(ImportExportImage.importImage(img));
+    /**
+     * Erodes given image
+     *
+     * @param img Image to be eroded
+     * @param itr Times erosion to be performed iteratively
+     * @return Eroded image
+     */
+    public BufferedImage erode(String img, int itr) {
+        return this.erode(ImportExportImage.importImage(img), itr);
     }
 }
