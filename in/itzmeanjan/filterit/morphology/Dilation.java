9a14983d680bcd7c56655ae8a07c602a6b3c88d3
Anjan Roy <anjanroy@yandex.com>
2020-05-17 12:15:17 +0530

    Added code doc in Dilation implementation

--- in/itzmeanjan/filterit/morphology/Dilation.java
+++ in/itzmeanjan/filterit/morphology/Dilation.java
@@ -5,8 +5,24 @@ import in.itzmeanjan.filterit.filter.ModeFilter;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Given an image with white foreground objects & black background color, it'll try to
+ * expand foreground area by applying a kernel on each pixel of image, which will make
+ * this pixel foreground ( white ) if at least one of the pixels present in its order-1
+ * neighbourhood is white colored i.e. we're extending foreground region by this morphological op.
+ * <p>
+ * This operation can also be helpful in detecting edges, by performing image subtraction.
+ */
 public class Dilation {
 
+    /**
+     * Dilation can be performed by simply applying mode filter
+     * ( picks high intensity pixel from neighbourhood ) iteratively for number of specified times.
+     *
+     * @param img Source image, to be dilated
+     * @param itr Number of times dilation to be performed
+     * @return Dilated image
+     */
     public BufferedImage dilate(BufferedImage img, int itr) {
         if (img == null || itr < 1) {
             return null;
@@ -19,6 +35,13 @@ public class Dilation {
         return sink;
     }
 
+    /**
+     * Applies dilation on image given as filepath
+     *
+     * @param img Source image, to be dilated
+     * @param itr Number of times dilation to be performed
+     * @return Dilated image
+     */
     public BufferedImage dilate(String img, int itr) {
         return this.dilate(ImportExportImage.importImage(img), itr);
     }
