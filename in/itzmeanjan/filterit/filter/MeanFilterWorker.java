7cd9dfb3f9d7c8da92e325f34e4e02320d819982
Anjan Roy <anjanroy@yandex.com>
2020-05-24 12:10:46 +0530

    Improved Mean filter
    - improved concurrency support in mean filter
    - now processing each row of image matrix using java thread pool

--- in/itzmeanjan/filterit/filter/MeanFilterWorker.java
+++ in/itzmeanjan/filterit/filter/MeanFilterWorker.java
@@ -1,70 +1,122 @@
 package in.itzmeanjan.filterit.filter;
 
-import in.itzmeanjan.filterit.Pixel;
+import in.itzmeanjan.filterit.segmentation.Image;
+import in.itzmeanjan.filterit.segmentation.Position;
+
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 /**
- * For concurrently applying mean filter on image pixels, we need to keep obtaining neighbourhood
- * pixel intensities for a given pixel P(x, y) & calculating mean value of them ( for all three
- * color components i.e. R, G & B ) in concurrent fashion. Which is what's done here. Multiple
- * excutions of `void run()` may run at a time on different thread, leading to faster processing of
- * whole image.
+ * Concurrently applies mean filter on a row of pixels in an image, I
  */
 class MeanFilterWorker implements Runnable {
 
-  private int order;
-  private Pixel pixel;
-  private BufferedImage src, sink;
+    private int order, row;
+    private Image src;
+    private BufferedImage sink;
 
-  MeanFilterWorker(BufferedImage src, BufferedImage sink, Pixel pixel, int order) {
-    this.src = src;
-    this.sink = sink;
-    this.pixel = pixel;
-    this.order = order;
-  }
-
-  /**
-   * Calculates mean of a set of integers belonging to [0, 255] range, returns rounded integer
-   * result
-   */
-  private int mean(int[][] pxlVal) {
-    int sum = 0;
-    for (int[] i : pxlVal) {
-      for (int j : i) {
-        sum += j;
-      }
+    MeanFilterWorker(int order, int row, Image src, BufferedImage sink) {
+        this.order = order;
+        this.row = row;
+        this.src = src;
+        this.sink = sink;
     }
-    return Math.round((float) sum / (float) Math.pow(pxlVal.length, 2));
-  }
 
-  /**
-   * Given a color component name i.e. one of {R, G, B}, it'll first obtain neighbourhood pixels
-   * intensity values for that color component; and compute mean of them, which will be set as pixel
-   * intensity of P(x, y) at sink image for this color component
-   *
-   * <p>That means, this function to be called thrice with R, G & B as color param's value.
-   */
-  private int getMeanForColorComponent(char color) {
-    return this.mean(this.pixel.getNeighbouringPixelsFromImage(this.src, color, this.order));
-  }
+    /**
+     * Calculates mean of a set of integers belonging to [0, 255] range,
+     * returns rounded integer result ( which is nothing but pixel intensity at I[x, y] )
+     */
+    private int mean(int[] pxlVal) {
+        int sum = 0;
+        for (int i : pxlVal) {
+            sum += i;
+        }
+        return Math.round((float) sum / (float) pxlVal.length);
+    }
 
-  /**
-   * Given a pixel & order of which neighbourhood around it to be obtained, it'll compute mean pixel
-   * intensity values for each of those color components ( i.e. R, G & B ), and finally update sink
-   * image
-   *
-   * <p>Assurance given, source image not to be modified
-   */
-  @Override
-  public void run() {
-    this.sink.setRGB(
-        this.pixel.posY,
-        this.pixel.posX,
-        new Color(
-                this.getMeanForColorComponent('r'),
-                this.getMeanForColorComponent('g'),
-                this.getMeanForColorComponent('b'))
-            .getRGB());
-  }
+    /**
+     * Extracts red color intensities from pixels, present in given neighbourhood
+     *
+     * @param positionArrayList Pixel information from neighbourhood of a pixel
+     * @return Set of red color intensities from that neighbourhood
+     */
+    private int[] getRedIntensities(ArrayList<Position> positionArrayList) {
+        int[] intensities = new int[positionArrayList.size()];
+        for (int i = 0; i < intensities.length; i++) {
+            intensities[i] = positionArrayList.get(i).getIntensityR();
+        }
+        return intensities;
+    }
+
+    /**
+     * Extracts green color intensities from pixels, present in given neighbourhood
+     *
+     * @param positionArrayList Pixel information from neighbourhood of a pixel
+     * @return Set of green color intensities from that neighbourhood
+     */
+    private int[] getGreenIntensities(ArrayList<Position> positionArrayList) {
+        int[] intensities = new int[positionArrayList.size()];
+        for (int i = 0; i < intensities.length; i++) {
+            intensities[i] = positionArrayList.get(i).getIntensityG();
+        }
+        return intensities;
+    }
+
+    /**
+     * Extracts blue color intensities from pixels, present in given neighbourhood
+     *
+     * @param positionArrayList Pixel information from neighbourhood of a pixel
+     * @return Set of blue color intensities from that neighbourhood
+     */
+    private int[] getBlueIntensities(ArrayList<Position> positionArrayList) {
+        int[] intensities = new int[positionArrayList.size()];
+        for (int i = 0; i < intensities.length; i++) {
+            intensities[i] = positionArrayList.get(i).getIntensityB();
+        }
+        return intensities;
+    }
+
+    /**
+     * Obtains neighbourhood of order X, around given pixel ( inclusive of itself )
+     *
+     * @param position Pixel information holder
+     * @return Set of pixels around given pixel ( neighbourhood of order X )
+     */
+    private ArrayList<Position> getNeighbourHood(Position position) {
+        return this.src.getNeighbourhoodOfOrderXInclusive(position, this.order);
+    }
+
+    /**
+     * Set of pixels in specified row of image matrix
+     *
+     * @return Pixels present in given row
+     */
+    private Position[] extractRow() {
+        return this.src.getPositions()[this.row];
+    }
+
+    /**
+     * Sets color at specified pixel location
+     *
+     * @param position Pixel information holder
+     * @param color    Color information
+     */
+    private void setIntensity(Position position, Color color) {
+        this.sink.setRGB(position.getX(), position.getY(), color.getRGB());
+    }
+
+    @Override
+    public void run() {
+        for (Position position : this.extractRow()) {
+            ArrayList<Position> positionArrayList = this.getNeighbourHood(position);
+            this.setIntensity(position,
+                    new Color(
+                            this.mean(this.getRedIntensities(positionArrayList)),
+                            this.mean(this.getGreenIntensities(positionArrayList)),
+                            this.mean(this.getBlueIntensities(positionArrayList))
+                    )
+            );
+        }
+    }
 }
